4dec8efe046b69b087fab98d65a72d468da985c8
trinity <ryan.cu1200@gmail.com>
2024-07-25 16:44:54 +0530

    initial commit and gui created

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QTextEdit, QPushButton, \
+    QVBoxLayout, QLineEdit, QLabel
+
+
+class QlineEdit:
+    pass
+
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUI()
+        self.setWindowTitle("Typing Speed Checker")
+
+    def setupUI(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.show_words = QLabel()
+        layout.addWidget(self.show_words)
+
+        self.input = QTextEdit()
+        self.input.setMinimumSize(500, 200)
+
+        self.input.setStyleSheet("padding: 5px; margin: 0px;")
+        layout.addWidget(self.input)
+
+        button_layout = QHBoxLayout()
+        layout.addLayout(button_layout)
+
+        self.start_button = QPushButton("Start")
+        button_layout.addWidget(self.start_button)
+
+        self.stop_button = QPushButton("Stop")
+        button_layout.addWidget(self.stop_button)
+
+        self.new_text_button = QPushButton("New Text")
+        button_layout.addWidget(self.new_text_button)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
